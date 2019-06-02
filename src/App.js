3e468b1953c9f5930ba19d03ebf61cda45ffc4f6
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2019-06-03 01:04:49 +0300

    very first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Contacts from "./components/Contacts";
+import Header from "./components/Header";
+import { Provider } from "./context";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
