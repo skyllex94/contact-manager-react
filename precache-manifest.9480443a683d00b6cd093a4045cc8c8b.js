4c7cfb00d444fbdb7f9aba56b737d5ca24791b99
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2019-06-04 14:40:49 +0300

    Updates

---
+++ precache-manifest.9480443a683d00b6cd093a4045cc8c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07675e1e85359b2228fc56a2a2fbab90",
+    "url": "/contact-manager-react/index.html"
+  },
+  {
+    "revision": "e739693a4e7c7a3e81a6",
+    "url": "/contact-manager-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "655073824672266c1c4e",
+    "url": "/contact-manager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e739693a4e7c7a3e81a6",
+    "url": "/contact-manager-react/static/js/2.a9a649e2.chunk.js"
+  },
+  {
+    "revision": "655073824672266c1c4e",
+    "url": "/contact-manager-react/static/js/main.0794daf7.chunk.js"
+  },
+  {
+    "revision": "b669651a203277be328c",
+    "url": "/contact-manager-react/static/js/runtime~main.3ce19021.js"
+  }
+]);
